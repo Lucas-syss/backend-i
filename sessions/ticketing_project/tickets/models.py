2f120fb180f0a7a629d69989005af9af6d831fe6
Lucas-syss <lucasguerreiroc@outlook.com>
2025-03-25 21:56:11 +0000

    feat: add project

---
+++ sessions/ticketing_project/tickets/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Ticket(models.Model):
+    STATUS_CHOICES = [('Open', 'Open'), ('Closed', 'Closed'), ('In Progress', 'In Progress')]
+    PRIORITY_CHOICES = [('Low', 'Low'), ('Medium', 'Medium'), ('High', 'High')] # use integer instead of string, to save power
+    
+    subject = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Open')
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='Medium')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+class Comment(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
